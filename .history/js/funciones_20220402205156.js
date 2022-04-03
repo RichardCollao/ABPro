598849e3373789e1905149853c9bf16e99572181
Richard Collao <richard.collao@outlook.cl>
2022-04-02 21:00:53 -0300

    Cambios menores en los textos y nuevos comentarios

--- .history/js/funciones_20220402205156.js
+++ .history/js/funciones_20220402205156.js
@@ -1,23 +1,22 @@
 // Contacto
 function validarFormulario(event) {
-  // Cancela el evento si este es cancelable, sin detener el resto del funcionamiento del evento, es decir, puede ser llamado de nuevo.
   event.preventDefault();
 
   var contact_name = document.getElementById('contact_name');
   if (contact_name.value.length < 3) {
-    alert('El campo "Nombre" debe contener al menos 3 letras.');
+    alert('El campo [Nombre] debe contener al menos 3 letras.');
     contact_name.focus();
     return;
   }
   var contact_email = document.getElementById('contact_email');
   if (contact_email.value.length < 6) {
-    alert('El campo "Correo" debe contener al menos 6 letras.');
+    alert('El campo [Correo] debe contener al menos 6 letras.');
     contact_email.focus();
     return;
   }
   var contact_comment = document.getElementById('contact_comment');
   if (contact_comment.value.length < 6) {
-    alert('El campo "Mensaje" debe contener al menos 6 letras.');
+    alert('El campo [Mensaje] debe contener al menos 6 letras.');
     contact_comment.focus();
     return;
   }
